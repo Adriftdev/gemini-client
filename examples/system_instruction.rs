6496b4b5c4d5fdf417b6857a135561290485e4da
Vasco Costa <vascomacosta@gmail.com>
2025-03-14 00:58:35 +0000

    feat: add example for system instruction

---
+++ examples/system_instruction.rs
@@ -0,0 +1,59 @@
+use gemini_client_rs::{
+    types::{GenerateContentRequest, PartResponse},
+    GeminiClient,
+};
+
+use dotenvy::dotenv;
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let api_key = std::env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY must be set");
+
+    let client = GeminiClient::new(api_key);
+    let model_name = "gemini-1.5-flash";
+
+    let req_json = json!({
+        "system_instruction": {
+            "parts": [
+                {
+                    "text": "You are Albert Einstein."
+                }
+            ],
+            "role": "system"
+        },
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": "Who are you?"
+                    },
+                    {
+                        "text": "What theories did you develop?"
+                    }
+                ],
+                "role": "user"
+            }
+        ],
+        "tools": []
+    });
+
+    let request: GenerateContentRequest = serde_json::from_value(req_json)?;
+
+    let response = client.generate_content(model_name, &request).await?;
+
+    let candidates = response.candidates.unwrap();
+
+    for candidate in &candidates {
+        for part in &candidate.content.parts {
+            match part {
+                PartResponse::Text(text) => println!("{}", text),
+                _ => { /* Ignore other part types as we are not using tools */ }
+            }
+        }
+    }
+
+    Ok(())
+}
